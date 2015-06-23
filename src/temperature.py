ca62f7cb5c8011fccc0080db2605914ce3b96682
Jeff Schriber <jbschri@emory.edu>
2015-06-23 09:49:51 -0400

    Fixed temperature conversion

--- src/temperature.py
+++ src/temperature.py
@@ -5,7 +5,7 @@ This is a python module that converts temperatures
 """
 
 def f_to_K(temp):
-    converted = ((temp - 32) * (5 / 9)) + 273.15
+    converted = ((temp - 32) * (5.0 / 9.0)) + 273.15
     return converted
 
 print f_to_K(212)
