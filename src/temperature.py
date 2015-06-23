1d3a68f29955cb62389890cc1376384c3675add2
Jeff Schriber <jbschri@emory.edu>
2015-06-23 10:00:04 -0400

    Added final temp conversions

--- src/temperature.py
+++ src/temperature.py
@@ -8,5 +8,13 @@ def f_to_K(temp):
     converted = ((temp - 32) * (5.0 / 9.0)) + 273.15
     return converted
 
-print f_to_K(212)
+def K_to_C(temp):
+    return temp - 273.15
+
+def f_to_C(temp):
+    return K_to_C(f_to_K(temp))
+
+
+print K_to_C(273.15)
+print f_to_C(32)
 
